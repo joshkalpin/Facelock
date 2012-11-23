e33ca6c180f153d95e7a71d48140b8fd4cceba35
kelly <kelly.ru4n@gmail.com>
2012-11-23 18:45:52 -0500

    Comments
    Signed-off-by: kelly <kelly.ru4n@gmail.com>

--- py/FaceRecognizer.py
+++ py/FaceRecognizer.py
@@ -19,27 +19,24 @@ class FaceRecognizer:
 
 	def analyse_picture(self):
 		while True:
-			img = self.inq.get(block=True)
-
+			img = self.inq.get(block=True)				## Block on inq until StillStarter
+														## sends an image
 			if img == None:
-				cv2.destroyAllWindows()
+				cv2.destroyAllWindows()					## Close down if it sends None
 				break
 
 			mat_img = cv.fromarray(img)
-
-			storage = cv.CreateMemStorage()
+														## Initialize Storage and find faces
+			storage = cv.CreateMemStorage()				## using haars.
 			cascade = cv.Load("./../haar/haarcascade_frontalface_alt.xml")
 			detected = cv.HaarDetectObjects(mat_img, cascade,
 				 storage, 1.2, 2,cv.CV_HAAR_DO_CANNY_PRUNING, (100,100))
 			if detected:
-				for face in detected:
-
+				for face in detected:					## Put each face detected in outq
+					cropped_image = cv.GetSubRect(mat_img,face[0])
+					self.outq.put(cropped_image)
 					#uncomment for green box
 					#cv.Rectangle(mat_img,p1,p2, cv.Scalar(0,255,0,0))
 
-					cropped_image = cv.GetSubRect(mat_img,face[0])
-
 					## Replace this line with something other than sums if you're interested.
 					## print sum(sum(sum(asarray(cropped_image))))
-
-					self.outq.put(cropped_image)
