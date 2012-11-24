b015a5c5b4bd80a93b7fe4b1b9a81942929e7917
Joshua Kalpin <joshua.kalpin@gmail.com>
2012-11-23 20:24:23 -0500

    User Controller for getting our users to and from the database

---
+++ py/UserLoader.py
@@ -0,0 +1,34 @@
+import sqlite3 as sql
+
+class User:
+
+	def __init__(self, uid, name, created, password):
+		self.uid = uid
+		self.name = name
+		self.created = created
+		self.password = password
+
+class UserController:
+
+	def __init__(self):
+		self.users = []
+		self.connection = sql.connect('./../db/facelock.db')
+		self.load_users()
+
+	def load_users(self):
+		cursor = self.connection.cursor()
+		for uid, name, created, password in zip(*cursor.execute("SELECT * FROM User")):
+			self.users.append(User(uid, name, created, password))
+
+		return self.users
+
+	def is_empty(self):
+		return self.users == []
+
+	def addUser(self, new_user):
+		cursor = self.connection.cursor()
+		cursor.execute("INSERT INTO User VALUES (%s, %s, %s, %s)" % (new_user.uid, new_user.name, new_user.created, new_user.password))
+
+	def removeUser(self, user):
+		cursor = self.connection.cursor()
+		cursor.execute("DELETE FROM User WHERE created = %s" % user.created)
